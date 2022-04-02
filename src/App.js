e9c6c3ae61f5105f0c7294b14360924747dc31dd
Thanh Vy <vythanh064@gmail.com>
2022-04-03 00:36:01 +0700

    hi

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import {useState} from 'react';
+// import "./App.css";
+// import React from 'react';
+// const gift = [
+//   'CPU i9',
+//   'RAM 16 RGB',
+//   'RGB keyboard'
+// ]
+// function App() {
+//   return(
+//     <div>
+//       <h2>Chưa có phần thưởng</h2>
+//       <button>Get</button>
+//     </div>
+//   )
+// }
+// export default App;
