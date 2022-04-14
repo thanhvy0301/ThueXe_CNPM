1b52cb4933bf36906e9ca1549f31854427bd8b96
Thanh Vy <vythanh064@gmail.com>
2022-04-15 00:43:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
-// import React from 'react';
-// import { Routes, Route, Link } from 'react-router-dom'
-// import { ReactDOM } from 'react';
-// import { BrowserRouter} from 'react-router-dom'
-// import Home from './Home';
-// import ThueXeTL from './ThueXETL';
-// function App() {
-//   return(
-//     <div>
-//         <h4>Thuê xe / Thuê xe tự lái</h4>
-//         <ul>
-//           <li>
-//             <Link to='/'>Home</Link>
-//           </li>
-//           <li>
-//           <Link to='/ThueXeTL'>ThueXeTuLai</Link>
-//           </li>          
-//         </ul>
-//         <Routes>
-//           <Route path="/" element={<Home/>} />
-//           <Route path="/ThueXeTL" element={<ThueXeTL/>} />
-//         </Routes>
-//     </div>
-//   )
-// }
-// export default App;
+import jquery from "jquery";
+function changeURL() {
+    location.replace("../public/ThueXeTL.html");
+}
