ca8eb5c381f8800bf69b4d112d72de69741bcfe1
Thanh Vy <vythanh064@gmail.com>
2022-04-14 14:00:29 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-// import {useState} from 'react';
-// import "./App.css";
-// import React from 'react';
-// const gift = [
-//   'CPU i9',
-//   'RAM 16 RGB',
-//   'RGB keyboard'
-// ]
-// function App() {
-//   return(
-//     <div>
-//       <h2>Chưa có phần thưởng</h2>
-//       <button>Get</button>
-//     </div>
-//   )
-// }
-// export default App;
+
+import React from 'react';
+function App() {
+  return(
+    <div>
+        <h4>Thuê xe / Thuê xe tự lái</h4>
+    </div>
+  )
+}
+export default App;
---
+++ src/ThueXETL.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ThueXeTL(){
+    return(
+        <div className='TX'>
+            <nav>
+                <ul>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                </ul>
+            </nav>
+            <index/>
+        </div>
+    )
+}
