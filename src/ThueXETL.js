4ca14802495b4b003209a1a4d457e8414d900f4d
Thanh Vy <vythanh064@gmail.com>
2022-04-15 10:06:37 +0700

    reUp

--- src/ThueXETL.js
+++ src/ThueXETL.js
@@ -1,14 +1,25 @@
-function myFunction() {
+// function myFunction() {
   
-    document.getElementById("myDropdown").classList.toggle("hidden");
-  }
+//     document.getElementById("myDropdown").classList.toggle("hidden");
+//   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (event.target.matches('.dropbtn')) {
-      document.getElementById("myDropdown").classList.toggle("show");
-    if(event.target.matches('.dropbtn')){
-       document.getElementById("myDropdown").classList.toggle("hidden");
-     }
-    }
-  }
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (event.target.matches('.dropbtn')) {
+//       document.getElementById("myDropdown").classList.toggle("show");
+//     if(event.target.matches('.dropbtn')){
+//        document.getElementById("myDropdown").classList.toggle("hidden");
+//      }
+//     }
+// }
+// function changSearch() {
+//   let btnChange = document.querySelector('.change-search-btn');
+//   let menuContent = document.querySelector('.menu-content');
+//   btnChange.addEventListener('click',()=>{
+//     if(menuContent.style.display===""){
+//        menuContent.style.display="block";
+//     } else {
+//        menuContent.style.display="";
+//     }
+//  })
+// }
