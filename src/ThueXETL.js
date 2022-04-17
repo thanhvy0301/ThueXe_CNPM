6dfcfffe9a82a3c9d31d733978e095dd50f64246
Thanh Vy <vythanh064@gmail.com>
2022-04-17 20:49:29 +0700

    change

--- src/ThueXETL.js
+++ src/ThueXETL.js
@@ -22,4 +22,13 @@
 //        menuContent.style.display="";
 //     }
 //  })
-// }
+// }'
+let dropdownBtn = document.querySelector('.menu-btn');
+  let menuContent = document.querySelector('.menu-content');
+  dropdownBtn.addEventListener('click',()=>{
+    if(menuContent.style.display===""){
+        menuContent.style.display="block";
+    } else {
+        menuContent.style.display="";
+    }
+  })
