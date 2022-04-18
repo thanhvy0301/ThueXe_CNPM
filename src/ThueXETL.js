0d1e8a34bd378c19589f12e6222746ec89941694
Thanh Vy <vythanh064@gmail.com>
2022-04-18 20:46:59 +0700

    check

--- src/ThueXETL.js
+++ src/ThueXETL.js
@@ -1,45 +1,20 @@
-// function myFunction() {
-  
-//     document.getElementById("myDropdown").classList.toggle("hidden");
-//   }
-  
-//   // Close the dropdown if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (event.target.matches('.dropbtn')) {
-//       document.getElementById("myDropdown").classList.toggle("show");
-//     if(event.target.matches('.dropbtn')){
-//        document.getElementById("myDropdown").classList.toggle("hidden");
-//      }
-//     }
+// import React from "react";
+// import faker from 'faker';
+// import '../Img' ;
+// import '../src/ThueXe.js';
+
+// const data = new Array(10000).fill().map((value, index) => ({ id: index, title: faker.lorem.words(5), body: faker.lorem.sentences(4) }))
+
+// function App() {
+//   return (
+//     <div>
+//       {data.map(((item) => (
+//         <div key={item.id} className="post">
+//           <h3>{item.title} - {item.id}</h3>
+//           <p>{item.body}</p>
+//         </div>
+//       )))}
+//     </div>
+//   );
 // }
-// function changSearch() {
-//   let btnChange = document.querySelector('.change-search-btn');
-//   let menuContent = document.querySelector('.menu-content');
-//   btnChange.addEventListener('click',()=>{
-//     if(menuContent.style.display===""){
-//        menuContent.style.display="block";
-//     } else {
-//        menuContent.style.display="";
-//     }
-//  })
-// }'
-// function dropDown() {
-//   let dropdownBtn = document.getElementsByClassName('menu-btn');
-//   let menuContent = document.getElementsByClassName('menu-content');
-//   let content = document.getElementById('content');
-//   let nav = document.getElementById('nav-head')
-//   dropdownBtn.addEventListener('click',()=>{
-//   if(menuContent.style.display=== ""){
-//       menuContent.style.display="block";
-//       content.style.marginTop = "250px";
-//       menuContent.style.borderBottomLeftRadius = "10px";
-//       menuContent.style.borderBottomRightRadius = "10px";           
-      
-//   } else {
-//       menuContent.style.display= "";
-//       content.style.marginTop = "20px";
-//       nav.style.borderBottomLeftRadius = "10px";
-//       nav.style.borderBottomRightRadius = "10px";
-//   }
-//   })
-// }
+// export default App;
