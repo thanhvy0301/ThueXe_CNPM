3e8808b69a31b88550477a173f634a7f68b1c385
Thanh Vy <vythanh064@gmail.com>
2022-04-19 23:30:17 +0700

    reCheck

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 
-import React from 'react';
+// import React from 'react';
+// function App() {
+//   return(
+//     <div>
+//         <h4>Thuê xe / Thuê xe tự lái</h4>
+//     </div>
+//   )
+// }
+// export default App;
+import React from "react";
+// import { ReactDOM } from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import { Routes, Route, Link  } from "react-router-dom";
+// import About from "./page/About";
+// import Contact from "./page/Contact";
+// import Home from "./page/Home/Home";
 function App() {
-  return(
-    <div>
-        <h4>Thuê xe / Thuê xe tự lái</h4>
-    </div>
-  )
+    return(
+        <div className="app">
+            {/* <ul>
+                <h2>Router</h2>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/About">About</Link>
+                </li>
+                <li>
+                    <Link to="/Contact">Contact</Link>
+                </li>
+            </ul> */}
+            {/* <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/About" element={<About/>}/>
+                <Route path="/Contact" element={<Contact/>}/>
+            </Routes>             */}
+        </div>
+    )
 }
 export default App;
